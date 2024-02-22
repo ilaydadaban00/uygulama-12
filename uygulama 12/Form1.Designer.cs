6e27b0e0d58fe439cc81a86670f4a9b4bd078d9a
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-02-23 00:29:03 +0300

    Add files via upload

---
+++ uygulama 12/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace uygulama_12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSAyi2 = new System.Windows.Forms.TextBox();
+            this.txtToplam = new System.Windows.Forms.TextBox();
+            this.btnheSAPLA = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "sayı1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "sayı2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "TOPLAM";
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(171, 28);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(100, 20);
+            this.txtSayi1.TabIndex = 3;
+            // 
+            // txtSAyi2
+            // 
+            this.txtSAyi2.Location = new System.Drawing.Point(171, 75);
+            this.txtSAyi2.Name = "txtSAyi2";
+            this.txtSAyi2.Size = new System.Drawing.Size(100, 20);
+            this.txtSAyi2.TabIndex = 4;
+            // 
+            // txtToplam
+            // 
+            this.txtToplam.Location = new System.Drawing.Point(171, 127);
+            this.txtToplam.Name = "txtToplam";
+            this.txtToplam.Size = new System.Drawing.Size(100, 20);
+            this.txtToplam.TabIndex = 5;
+            // 
+            // btnheSAPLA
+            // 
+            this.btnheSAPLA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnheSAPLA.Location = new System.Drawing.Point(74, 185);
+            this.btnheSAPLA.Name = "btnheSAPLA";
+            this.btnheSAPLA.Size = new System.Drawing.Size(173, 73);
+            this.btnheSAPLA.TabIndex = 6;
+            this.btnheSAPLA.Text = "hesapla";
+            this.btnheSAPLA.UseVisualStyleBackColor = false;
+            this.btnheSAPLA.Click += new System.EventHandler(this.btnheSAPLA_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(425, 450);
+            this.Controls.Add(this.btnheSAPLA);
+            this.Controls.Add(this.txtToplam);
+            this.Controls.Add(this.txtSAyi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSAyi2;
+        private System.Windows.Forms.TextBox txtToplam;
+        private System.Windows.Forms.Button btnheSAPLA;
+    }
+}
+
